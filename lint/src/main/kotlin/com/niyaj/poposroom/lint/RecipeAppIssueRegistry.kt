9209dc7f8decfb475ecd5ab06d9c78007b77f1f7
Sk Niyaj Ali <niyaj639@gmail.com>
2024-05-12 13:57:35 +0530

    feat - Added lint module
    - Close #3

---
+++ lint/src/main/kotlin/com/niyaj/poposroom/lint/RecipeAppIssueRegistry.kt
@@ -0,0 +1,43 @@
+/*
+ *
+ *  * Copyright 2024 Sk Niyaj Ali
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.niyaj.poposroom.lint
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.client.api.Vendor
+import com.android.tools.lint.detector.api.CURRENT_API
+import com.niyaj.poposroom.lint.designsystem.DesignSystemDetector
+
+class RecipeAppIssueRegistry : IssueRegistry() {
+
+    override val issues = listOf(
+        DesignSystemDetector.ISSUE,
+        TestMethodNameDetector.FORMAT,
+        TestMethodNameDetector.PREFIX,
+    )
+
+    override val api: Int = CURRENT_API
+
+    override val minApi: Int = 12
+
+    override val vendor: Vendor = Vendor(
+        vendorName = "Popos Room",
+        feedbackUrl = "https://github.com/niyajali/RecipeApp/issues",
+        contact = "https://github.com/niyajali/RecipeApp",
+    )
+}
